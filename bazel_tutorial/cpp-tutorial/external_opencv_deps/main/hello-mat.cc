1cd61885f1a177a68f53737d5ea8b5ead6cc38aa
chenyiliang <yiliang@huahuico.com>
2021-07-12 17:10:34 +0800

    compile face detecion with bazel

--- bazel_tutorial/cpp-tutorial/external_opencv_deps/main/hello-mat.cc
+++ bazel_tutorial/cpp-tutorial/external_opencv_deps/main/hello-mat.cc
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include "test_face_detection.hpp"
 
 int main() {
-    cv::Mat mat = (cv::Mat_<double>(3, 3) << 1,2,3,4,5,6,7,8,9);
-    std::cout << "hell mat = " << std::endl << mat << std::endl;
+    // cv::Mat mat = (cv::Mat_<double>(3, 3) << 1,2,3,4,5,6,7,8,9);
+    // std::cout << "hell mat = " << std::endl << mat << std::endl;
+    faceDetection();
     return 0;
 }
---
+++ bazel_tutorial/cpp-tutorial/external_opencv_deps/main/test_face_detection.hpp
@@ -0,0 +1,26 @@
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/objdetect.hpp>
+#include <iostream>
+
+void faceDetection() {
+    std::string path = "/Users/mac/OpenCV/MyOpenCVAI/MyOpenCVAI/resources/faces_detection_2.jpeg";
+    cv::Mat img = cv::imread(path);
+    cv::CascadeClassifier faceCascade;
+    faceCascade.load("/Users/mac/OpenCV/MyOpenCVAI/MyOpenCVAI/resources/haarcascade_frontalface_default.xml");
+    if (faceCascade.empty())
+    {
+        std::cout << "XML file not loaded" << std::endl;
+        return;
+    }
+    std::vector<cv::Rect> faces;
+    faceCascade.detectMultiScale(img, faces, 1.1, 10);
+    for (size_t i = 0; i < faces.size(); i++)
+    {
+        cv::rectangle(img, faces[i].tl(), faces[i].br(), cv::Scalar(255, 0, 255), 2);
+    }
+    cv::imshow("face detection", img);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+}
